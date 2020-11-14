b661db5f508e476c1539268179e213be54d74b90
Erick Recher <javascriptforlife@gmail.com>
2020-11-14 00:29:17 -0800

    jQuery addded functionatlity

---
+++ javascript.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  //change red
+  $("change-red").click(function(){
+
+  });
+  //change green
+  "change-green"
+  //change blue
+  "change-blue"
+  //make big
+  "make-big"
+  //make small
+  "make-small"
+  //animate div
+  "animate-div"
+  //.hide() and .show()
+});

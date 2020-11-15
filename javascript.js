dde9d2d417b86464d78c87ea60e329ab499e8e9b
Erick Recher <javascriptforlife@gmail.com>
2020-11-14 23:31:41 -0800

    jquery in progress

--- javascript.js
+++ javascript.js
@@ -1,17 +1,25 @@
 $(document).ready(function(){
   //change red
-  $("change-red").click(function(){
+  console.log("Hello");
+  $("change-red").on(click, function(){
 
   });
+  $(document).on("click", "#change-red", function(){
+    $("btn btn-danger btn-block").css("background-color", "green");
+  });
   //change green
-  "change-green"
+  $(document).on("click", "#change-green", function(){
+    $("btn btn-success btn-block").css("background-color", "red");
+  });
   //change blue
-  "change-blue"
-  //make big
-  "make-big"
-  //make small
-  "make-small"
-  //animate div
-  "animate-div"
-  //.hide() and .show()
+  $(document).on("click", "change-blue", function(){
+    $("btn btn-primary btn-block").css("background-color", "red");
+  });
+//make big
+//   "make-big"
+//make small
+//   "make-small"
+//animate div
+//   "animate-div"
+//.hide() and .show()
 });
